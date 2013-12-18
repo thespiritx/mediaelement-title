a2e67d24217f06a0a7d46c0175cc64fc178cf1fc
phuongdh <phuongdh@gmail.com>
2013-12-18 04:41:45 -0500

    Fixed title so it works in fullscreen and on top of white videos

--- app/assets/javascripts/mediaelement-title/mep-feature-title.js
+++ app/assets/javascripts/mediaelement-title/mep-feature-title.js
@@ -1,14 +1,22 @@
 (function($) {
         $.extend(mejs.MepDefaults, {
-		title: null,
-		titleLink: null
+                title: null,
+                titleLink: null
         });
 
         $.extend(MediaElementPlayer.prototype, {
-		buildtitle: function(player, controls, layers, media) {
+                buildtitle: function(player, controls, layers, media) {
                         if (player.isVideo && player.options.title != null ) {
-				$('<div class="mejs-overlay mejs-overlay-title"><a href="' + player.options.titleLink + '">' + player.options.title + '</a></div>').appendTo(layers)
+                                title = $('<div class="mejs-title"><a href="' + player.options.titleLink + '">' + player.options.title + '</a></div>');
+                                title.insertAfter(layers);
+                                media.addEventListener('play',function() {
+                                        title.hide();
+                                }, false);
+                                media.addEventListener('pause',function() {
+                                        title.show();
+                                }, false);
                         }
                 }
         });
 })(mejs.$);
+
