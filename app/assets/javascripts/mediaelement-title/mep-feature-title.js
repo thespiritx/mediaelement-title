9382366930a5b5bf6583aae85c2317cbc21dc5a3
Clinton LaForest <tsxcom@gmail.com>
2017-05-12 11:58:44 -0400

    Update namespace to support mediaelement 3.x

--- app/assets/javascripts/mediaelement-title/mep-feature-title.js
+++ app/assets/javascripts/mediaelement-title/mep-feature-title.js
@@ -7,7 +7,7 @@
         $.extend(MediaElementPlayer.prototype, {
                 buildtitle: function(player, controls, layers, media) {
                         if (player.isVideo && player.options.title != null ) {
-                                title = $('<div class="mejs-title"><a href="' + player.options.titleLink + '">' + player.options.title + '</a></div>');
+                                title = $('<div class="mejs__title"><a href="' + player.options.titleLink + '">' + player.options.title + '</a></div>');
                                 title.insertAfter(layers);
                                 media.addEventListener('play',function() {
                                         title.hide();
